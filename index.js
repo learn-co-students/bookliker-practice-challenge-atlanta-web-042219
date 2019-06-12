8e8ac23753f85723a9e922523289c99e60a0a157
ascotttoney <ascotttoney@gmail.com>
2019-06-12 11:51:44 -0400

    Done.

--- index.js
+++ index.js
@@ -1,62 +1,89 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", () => {
+  getBooks()
+  const form = document.getElementById("add-book-form")
+  form.addEventListener("submit", createBook)
+})
 
-  const baseURL = `http://localhost:3000/books`
-  document.addEventListener('click', handleClickEvents)
+function getBooks() {
+  fetch("http://localhost:3000/books")
+  .then(res => res.json())
+  .then(res => 
+      {res.forEach(book => populateBook(book))
+  })
+}
 
-  fetchBooks()
+function createBook(e) {
+  e.preventDefault()
+  const form = document.getElementById("add-book-form")
+  fetch("http://localhost:3000/books", {
+    method: "POST",
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      "title": e.target.title.value, 
+      "img_url": e.target.img_url.value, 
+      "description": e.target.description.value, 
+      "likes": 0
+    })
+  }).then(resp => resp.json())
+  .then(populateBook)
+  .then(form.reset())
+}
 
-  function fetchBooks() {
-    fetch(baseURL)
-    .then(res => res.json())
-    // .then(console.log)
-    .then(json => json.forEach(listBook))
-  }
+function deleteBook(e) {
+  fetch(`http://localhost:3000/books/${e.target.dataset.bookId}`, {
+    method: "DELETE",
+    headers: {"Content-Type":"application/json"}
+  })
+  .then(e.target.parentElement.remove())
+  .then(document.getElementById(e.target.dataset.bookId).remove())
+}
 
-//----------------------------------------------------------------------------//
 
-  function listBook(book) {
-    console.log(book.users)
+function populateBook(book) {
+  const ul = document.getElementById("list")
+  const li = document.createElement("li")
 
-    const ul = document.getElementById('list')
-    const li = document.createElement('li')
+  li.id = book.id
+  li.innerText = book.title
 
-    li.innerText = book.title
-    li.addEventListener('click', function(){
-      const div = document.getElementById('show-panel')
+  li.addEventListener("click", () => {
+    const showPanel = document.getElementById("show-panel")
 
-      div.innerHTML =
-        `<h2>${book.title}</h2>
-        <img src="${book.img_url}"/>
-        <p>${book.description}</p>
-        <button id='read-book-button'>Read Book</button>
-        <div id='data-set-div' data-book-id='${book.id}' data-users='${book.users}'></div>`
+    const h1 = document.createElement("h1")
+    h1.innerText = book.title
+
+    const img = document.createElement("img")
+    img.src = book.img_url
+
+    const p = document.createElement("p")
+    p.innerText = book.description
+
+    const likeButton = document.createElement("button")
+    likeButton.innerText = "Like...Subscribe: "
+
+    const btn_span = document.createElement("span")
+    btn_span.innerText = book.likes
+    likeButton.append(btn_span)
+
+    likeButton.addEventListener("click", () => {
+      btn_span.innerText = parseInt(btn_span.innerText) + 1
+      fetch(`http://localhost:3000/books/${book.id}`,{
+        method: "PATCH",
+        headers: {"Content-Type":"application/json"},
+        body: JSON.stringify({"likes": btn_span.innerText})
+      })
     })
 
-    ul.appendChild(li)
+    const deleteButton = document.createElement("button")
 
-  }
+    deleteButton.addEventListener("click", deleteBook)
+    deleteButton.innerText = "Delete Book"
+    deleteButton.id = "delete"
+    deleteButton.dataset.bookId = book.id
 
-//----------------------------------------------------------------------------//
+    showPanel.innerHTML = ""
+    showPanel.append(h1, img, p, likeButton, deleteButton)  
+  })
 
-  function handleClickEvents(e) {
-      console.log(e.target)
-    if (e.target.id === 'read-book-button') readBookClicked(e.target.nextSibling.nextSibling)
-    // e.target is the button / nextSibling is the button text / sibling after that is the div
-  }
-
-//----------------------------------------------------------------------------//
-
-  function readBookClicked(id) {
-    // console.log('readBookClicked', id)
-
-    // fetch(`${baseURL}`/`${id}`,{
-    //   method: 'PATCH',
-    //   headers: { 'Content-Type': 'application/json'},
-    //   body: JSON.stringify({ })
-    // })
-    
-  }
-
-// FINAL BRACKET -------------------------------------------------------------//
-
-})
+  ul.append(li)
+}
